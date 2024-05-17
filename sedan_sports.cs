616254944e2e0014b700f26c705a0e1f24b3caeb
brisdalen <bwendelborgrisdalen@gmail.com>
2024-05-17 22:32:54 +0200

    Imported tons of car objects. Set up a sedan with collisions, ground plane with the standard movement script

---
+++ sedan_sports.cs
@@ -0,0 +1,40 @@
+using Godot;
+using System;
+
+public partial class sedan_sports : CharacterBody3D
+{
+	public const float Speed = 5.0f;
+	public float WheelBase = 0.6f;
+	public float SteeringLimit = 10.0f;
+	public float EnginePower = 6.0f;
+
+	// Get the gravity from the project settings to be synced with RigidBody nodes.
+	public float gravity = ProjectSettings.GetSetting("physics/3d/default_gravity").AsSingle();
+
+	public override void _PhysicsProcess(double delta)
+	{
+		Vector3 velocity = Velocity;
+
+		// Add the gravity.
+		if (!IsOnFloor())
+			velocity.Y -= gravity * (float)delta;
+
+		// Get the input direction and handle the movement/deceleration.
+		// As good practice, you should replace UI actions with custom gameplay actions.
+		Vector2 inputDir = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
+		Vector3 direction = -(Transform.Basis * new Vector3(inputDir.X, 0, inputDir.Y)).Normalized();
+		if (direction != Vector3.Zero)
+		{
+			velocity.X = direction.X * Speed;
+			velocity.Z = direction.Z * Speed;
+		}
+		else
+		{
+			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
+			velocity.Z = Mathf.MoveToward(Velocity.Z, 0, Speed);
+		}
+
+		Velocity = velocity;
+		MoveAndSlide();
+	}
+}
